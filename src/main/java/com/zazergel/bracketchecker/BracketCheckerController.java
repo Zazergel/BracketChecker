3401b00e33accb31ae75a72e6e3f25ad9ae639c6
Zazergel <freewarcat@yandex.ru>
2024-01-09 15:49:33 +0300

    v2.4
    refactor: рефактор аннотаций в классе BracketCheckerController
    fix: Исправлена опечатка в сообщении аннотации Size класса BracketsRequest, теперь кол-во символов в сообщении совпадает с реальным ограничением
    fix: Исправлен основной алгоритм проверки текста в классе BracketCheckerServiceImpl, улучшена производительность.

--- src/main/java/com/zazergel/bracketchecker/BracketCheckerController.java
+++ src/main/java/com/zazergel/bracketchecker/BracketCheckerController.java
@@ -4,9 +4,8 @@ import com.zazergel.bracketchecker.model.BracketsRequest;
 import com.zazergel.bracketchecker.model.BracketsResponse;
 import com.zazergel.bracketchecker.service.BracketCheckerService;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,10 +14,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api")
+@AllArgsConstructor
 @Slf4j
-@RequiredArgsConstructor
 public class BracketCheckerController {
-    @Autowired
+
     BracketCheckerService bracketCheckerService;
 
     @PostMapping("/checkBrackets")
